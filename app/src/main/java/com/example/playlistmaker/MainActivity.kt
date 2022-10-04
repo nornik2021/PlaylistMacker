e62648c4f281121a163b65d45270d48791c4289f
nornik2021 <nornik2019@gmail.com>
2022-10-04 23:08:50 +0300

    init

---
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.playlistmaker
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val displayButton1 = findViewById<Button>(R.id.button1)
+        val displayButton2 = findViewById<Button>(R.id.button2)
+        val displayButton3 = findViewById<Button>(R.id.button3)
+
+        displayButton1.setOnClickListener {
+            val displayIntent1 = Intent(this, SearchActivity::class.java)
+            startActivity(displayIntent1)
+        }
+        displayButton2.setOnClickListener {
+            val displayIntent2 = Intent(this, MediaActivity::class.java)
+            startActivity(displayIntent2)
+        }
+
+        displayButton3.setOnClickListener {
+            val displayIntent3 = Intent(this, SettingsActivity::class.java)
+            startActivity(displayIntent3)
+        }
+
+    }
+}
