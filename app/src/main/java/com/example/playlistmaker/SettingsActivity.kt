77f4c2b519206fd32be0d24fb8c932b74dc684f4
nornik2021 <nornik2019@gmail.com>
2022-10-28 11:24:14 +0300

    4-attemps

--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -6,6 +6,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageButton
+import android.widget.Toast
+import java.lang.Exception
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,14 +23,33 @@ class SettingsActivity : AppCompatActivity() {
                 Uri.parse("https://yandex.ru/legal/practicum_offer/"))
             startActivity(browseIntent)
         }
-        settingsButtonShare.setOnClickListener {
-            val settingsIntentShare = Intent(this, ShareActivity::class.java)
-            startActivity(settingsIntentShare)
-        }
-        settingsButtonDialog.setOnClickListener {
-            val settingsIntentDialog = Intent(this, DialogActivity::class.java)
-            startActivity(settingsIntentDialog)
 
+
+        settingsButtonShare.setOnClickListener {
+            val mIntent = Intent(Intent.ACTION_SEND)
+            mIntent.data = Uri.parse("mailto:")
+            mIntent.type = "text/plain"
+            mIntent.putExtra(Intent.EXTRA_EMAIL, getString(R.string.nornik2019_gmail_com))
+            mIntent.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.playlist_maker))
+            mIntent.putExtra(Intent.EXTRA_TEXT,getString(R.string.Spasibo))
+
+            try {
+                startActivity(Intent.createChooser(mIntent,"Выберите почтовый клиент...."))
+            }
+            catch (e: Exception){
+                Toast.makeText(this,e.message,Toast.LENGTH_LONG).show()
+            }
+        }
+
+
+        settingsButtonDialog.setOnClickListener {
+            val intent = Intent(Intent.ACTION_SEND).apply {
+                type = "*/*"
+                putExtra(Intent.EXTRA_TEXT,getString(R.string.practicum_yandex_))
+            }
+            if (intent.resolveActivity(packageManager)!=null){
+                startActivity(intent)
+            }
         }
     }
 }
